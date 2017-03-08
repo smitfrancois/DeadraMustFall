a329d273c9d3b8c28d82da2a4ab6b0b0af9c5698
Francois Smit <smit.fran@gmail.com>
2017-03-08 07:45:27 +0200

    Added DBContext for CodeFirst Model

---
+++ DeadraMustFall.Model/DeadraMustFallContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeadraMustFall.Model
+{
+    public class DeadraMustFallContext:DbContext
+    {
+        public DeadraMustFallContext() : base("name=DeadraMustFallDBConnectionString")
+        {
+            Database.SetInitializer<DeadraMustFallContext>(new DropCreateDatabaseIfModelChanges<DeadraMustFallContext>());
+            Database.Initialize(true);
+        }
+    }
+}
